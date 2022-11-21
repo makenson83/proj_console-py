4921378ff1b0dc13cd07d3e9ca1ecc20a72e8ee4
Makenson <simolenmakenson83@gmail.com>
2022-11-21 20:51:05 -0300

    addicionando interface com usuario e ajustes

--- aula 02/CadastroVeiculo.py
+++ aula 02/CadastroVeiculo.py
@@ -1,4 +1,6 @@
-class veiculo:
+import os
+
+class veículo:
     def __init__(self, marca, modelo, ano, placa):
         self.marca = marca
         self.modelo = modelo
@@ -8,10 +10,9 @@ class veiculo:
 
 cadastro = []
 x = 0
+def clear(): os.system('clear')
 
-
-def tela():
-    def clear(): return os.system('clear')
+def tela():  
     clear()
     print("_________________________________")
     print("__________cadastro de veiculo_________")
@@ -25,5 +26,21 @@ def tela():
     y = 0
     while (y <= x):
         for v in cadastro:
-            print("{:<14} {:<8} {:<19} {:<9}".format(
+            print("{:<14} {:<8} {:<10} {:<9}".format(
                 v.marca, v.modelo, v.ano, v.placa))
+            y += 1
+
+
+res = int(input("\ndeseja cadastrar um novo veiculo? sim(1) / não(0) "))
+
+while (res == 1):
+   
+    cadmarca = input("\ndigite a marca do veículo ")
+    cadmodelo = input("digite o modelo do veículo ")
+    cadano = input("digite o ano do veículo ")
+    cadplaca = input("digite a placa do veículo ")
+    cadastro.append(veículo(cadmarca, cadmodelo, cadano, cadplaca))
+    tela()
+    x += 1
+
+    res = int(input("\ndeseja cadastrar um novo veículio? sim(1) / não(0)"))
