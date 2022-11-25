8282e9670ec4efc76d910ee097816e70236c88ea
Makenson <simolienmakenson83@gmail.com>
2022-11-25 20:06:30 -0300

    ajustando interface da calculadora

--- aula 03/calculadorasimples.py
+++ aula 03/calculadorasimples.py
@@ -15,13 +15,16 @@ calculadora.append(operador(3,"x","multiplicação"))
 calculadora.append(operador(4,"÷","divição"))
 
 def calc():
-    print("|{:<3}|{:<4}|{:<14}|".format("Nº","Op","informação"))
-    print("|--------------------------|")
+    print("==========================")
+    print("|{:<3}|{:<4}|{:<15}|".format("Nº","Op","informação "))
+    print("|---+----+---------------|")
     for v in calculadora:
         print("|{:<3}|{:<4}|{:<15}|".format(v.opcao, v.operador, v.info))
+    print("==========================")
 while(x==0):
+    clear()
     calc()
-    res = int(input("\nselecione o numero da operação que realizar o 0 para sair"))
+    res = int(input("\nselecione o numero da operação que realizar: "))
 
     class calculo:
         def switch(self,operacao):
@@ -29,21 +32,25 @@ while(x==0):
             return getattr(self, 'case_'+str(operacao), lambda:default)()
         def case_1(self):
             v1 = int(input("\ndigite o valor 1: "))
-            v2 = int(input("\ndigite o valor 1: "))
+            v2 = int(input("digite o valor 2: "))
             return v1+v2
         def case_2(self):
             v1 = int(input("\ndigite o valor 1: "))
-            v2 = int(input("\ndigite o valor 1: "))
+            v2 = int(input("digite o valor 2: "))
             return v1-v2
         def case_3(self):
             v1 = int(input("\ndigite o valor 1: "))
-            v2 = int(input("\ndigite o valor 1: "))
+            v2 = int(input("digite o valor 2: "))
             return v1*v2
         def case_4(self):
             v1 = int(input("\ndigite o valor 1: "))
-            v2 = int(input("\ndigite o valor 1: "))
+            v2 = int(input("digite o valor 2: "))
             return v1/v2
     s = calculo() 
-    print("\n",s.switch(res))
+    print("\n o valor resultante é: ",s.switch(res))
+    x=int(input("\nDeseja realizar outra operação? sim (0) ou não (1): "))
+clear()
+
+    
 
 
