c31aacab1ce2e96578757ddfb4f452b52be6bb48
Makenson <simolienmakenson83@gmail.com>
2022-12-09 18:58:12 -0300

    adicionando diesel s-10

--- aula 05/Combutivel.py
+++ aula 05/Combutivel.py
@@ -31,11 +31,11 @@ def aditivada(opcao):
         pagamento = int(input("\ndigite 1 pra o total em reais \ndigite 2 para total em litros:\n"))
         if(pagamento==1):
             valor=int(input("\ndigite o total em reais : "))
-            total = float(valor/4.90)
+            total = float(valor/4.99)
             return print("o valor total em litros de R$",valor,valor ," é de  " ,total," litros.")
         else:
             litros=int(input("\ndigite o total em litros: "))
-            total = float(litros*4.90)
+            total = float(litros*4.99)
             return print("o valor total em reais de ",litros,"é de R$",total,".")
 
 def s500(opcao):
@@ -50,3 +50,17 @@ def s500(opcao):
             litros=int(input("\ndigite o total em litros: "))
             total = float(litros*6.59)
             return print("o valor total em reais de ",litros,"é de R$",total,".")
+
+def s10(opcao):
+        print("\nCódigo selecionado: - ",opcao)
+        print("Produto selecionado: diesel s10")
+        pagamento = int(input("\ndigite 1 pra o total em reais \ndigite 2 para total em litros:\n"))
+        if(pagamento==1):
+            valor=float(input("\ndigite o total em reais : "))
+            total = float(valor/6.20)
+            return print(f'o valor total em litros de R${valor: .2f} é de {total: .2f} litros.')
+        else:
+            litros = float(input("\ndigite o total em litros: "))
+            total = float(litros*6.20)
+            return print(f'o valor total em reais de {litros: .2f}litros é de R${total: .2F}')
+    
